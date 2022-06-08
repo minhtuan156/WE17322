78bf5ecd1edcc6c3ef8f9b3c7038e1043a1477ca
sontv8 <sontv8@fpt.edu.vn>
2022-06-08 14:43:08 +0700

    product page

--- Assignment/src/main.js
+++ Assignment/src/main.js
@@ -37,7 +37,7 @@ const ListProduct = [
         price:60,
         desc:"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
         image:"./image/home/popular-item-2.png",
-        category: 5
+        category: 3
     },
     {
         id: 6,
@@ -45,7 +45,7 @@ const ListProduct = [
         price:15,
         desc:"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
         image:"./image/home/popular-item-3.png",
-        category: 6
+        category: 3
     }
 ]
 const ListCategory = [
@@ -134,24 +134,73 @@ if(filterSelect){
 function detailProduct(){
     const prdId = new URLSearchParams(window.location.search).get('id')
     // console.log(prdId);
-    const product = ListProduct.find(function(item){
-        return item.id == prdId
-    })
+    if(prdId){
+        const product = ListProduct.find(function(item){
+            return item.id == prdId
+        })
+        
+        const detailProductDiv = document.querySelector('.detail-product')
+        detailProductDiv.innerHTML = `
+            <div class="product-info">
+                <h2>${product.name}</h2>
+                <p class="price">$ ${product.price}</p>
+                <p class="desc">${product.desc}</p>
+                <form action="">
+                    <input type="text" placeholder="Quantity">
+                    <button type="submit">Add To Cart</button>
+                </form>
+            </div>
+            <div class="product-img">
+                <img src="${product.image}" alt="">
+            </div>
+        `
+    }
     
-    const detailProductDiv = document.querySelector('.detail-product')
-    detailProductDiv.innerHTML = `
-        <div class="product-info">
-            <h2>${product.name}</h2>
-            <p class="price">$ ${product.price}</p>
-            <p class="desc">${product.desc}</p>
-            <form action="">
-                <input type="text" placeholder="Quantity">
-                <button type="submit">Add To Cart</button>
-            </form>
-        </div>
-        <div class="product-img">
-            <img src="${product.image}" alt="">
-        </div>
-    `
 }
 detailProduct()
+
+function listCategory(){
+    const cateList = document.querySelector('#cate-list')
+    for(let item of ListCategory){
+        cateList.innerHTML += `
+            <li><a href="./products.html?cateId=${item.id}">${item.name}</a></li>
+        `
+    }
+}
+listCategory()
+
+function listProductPage(data){
+    const listProductDiv = document.querySelector('#list-product')
+    listProductDiv.innerHTML = ""
+    for(let item of data){ // lặp qua mảng truyền vào tham số data
+
+        // đổ dữ liệu vào trong thẻ div popular
+        listProductDiv.innerHTML += `
+            <div class="popular__item">
+                <a href="./detail.html?id=${item.id}"><img src="${item.image}" alt=""></a>
+                <h3><a href="./detail.html?id=${item.id}">${item.name}</a></h3>
+                <p>
+                    <img src="./image/home/Timer.png" alt="">
+                    <span>30 Minutes</span>
+                    <img src="./image/home/ForkKnife.png" alt="">
+                    <span>Snack</span>
+                </p>
+            </div>
+        `         
+    }
+}
+
+
+function reRender(){
+    const cateId = new URLSearchParams(window.location.search).get('cateId')
+    const productByCategory = ListProduct.filter(function(item){
+        return item.category == cateId
+    })   
+    if(cateId){
+        listProductPage(productByCategory)
+    }else{
+        listProductPage(ListProduct)
+    }
+    
+}
+reRender()
